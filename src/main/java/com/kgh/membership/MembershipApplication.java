957a0e3656a55fba54e37692838fe75f8004592c
gwanhyeonkim <gwanhyeon0525@gmail.com>
2022-08-03 17:50:33 +0900

    feat: log4j2 config

--- src/main/java/com/kgh/membership/MembershipApplication.java
+++ src/main/java/com/kgh/membership/MembershipApplication.java
@@ -1,13 +1,20 @@
 package com.kgh.membership;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MembershipApplication {
 
+public class MembershipApplication {
+	private static Logger logger = LogManager.getLogger(MembershipApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(MembershipApplication.class, args);
+		logger.info("info = {}", "info Test");
+		logger.debug("debug = {}", "debug Test");
+		logger.warn("warn = {}", "warn Test");
+		logger.error("error = {}", "error Test");
 	}
 
 }
