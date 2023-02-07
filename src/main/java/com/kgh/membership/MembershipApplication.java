83317e7eead13138091dde55ce5e31bb8c1e0f8c
gwanhyeon <gwanhyeon0525@gmail.com>
2023-02-07 16:35:51 +0900

    통신사 조회/전체조회/생성 TDD 적용 (#19)
    * feat: junit 테스트 세팅 및 log4j2 적용
    
    * feat: 통신사 유저 조개별조회 / 전체조회 / 생성 TDD 적용
    
    * fix: JDK8 LocalDate 날짜 API 변경

--- src/main/java/com/kgh/membership/MembershipApplication.java
+++ src/main/java/com/kgh/membership/MembershipApplication.java
@@ -1,7 +1,5 @@
 package com.kgh.membership;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,9 +11,7 @@ import javax.persistence.EntityListeners;
 @EntityListeners(AuditingEntityListener.class)
 @EnableJpaAuditing
 public class MembershipApplication {
-	private static Logger logger = LogManager.getLogger(MembershipApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(MembershipApplication.class, args);
-		logger.info("info = {}", MembershipApplication.class);
 	}
 }
